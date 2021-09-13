654762e3e3ec52623c4116f8d288c66e08c0994e
Bbenz <bbenz.zou@elitesland.com>
2021-09-13 10:26:17 +0800

    # 添加springBean的Demo

---
+++ src/main/java/com/el/ureport/model/spirngbean/User.java
@@ -0,0 +1,31 @@
+package com.el.ureport.model.spirngbean;
+
+public class User {
+    private int id;
+    private String name;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
